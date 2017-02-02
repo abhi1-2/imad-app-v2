b3e737ac8c35d40d2725e12ebd7128a47d665049
abhi1-2 <abhishek.sarkar698@gmail.com>
2017-02-03 00:40:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1',function(req,res){
-   res.send(createTemplate(articles[article1]));
+   res.send(createTemplate(articles(article1)));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
