a1d590a98b73810f41d497865f8f860d54d37000
abhi1-2 <abhishek.sarkar698@gmail.com>
2017-02-02 22:29:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,21 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('article1',function(req,res){
+    res.send("article1 requested and will be served");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('article2',function(req,res){
+    res.send("article2 requested and will be served");
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+app.get('article3',function(req,res){
+    res.send("article3 requested and will be served");
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
