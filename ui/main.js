9db548608d7f6c16b91d504a619a436a990a4f61
abhi1-2 <abhishek.sarkar698@gmail.com>
2017-02-04 13:12:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ var element= document.getElementById('aat');
 element.innerHTML="qert";
 var pic=document.getElementById('er');
 marginleft=0;
-var movrit=function(){
+var moveRight=function(){
   var marginleft=marginleft+15;
   pic.style.marginleft=marginleft+'px';
 };
 pic.onclick=function(){
-  var imt=setInterval(movrit,50);  
+  var imt=setInterval(moveRight,50);  
 };
