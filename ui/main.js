cca3740f0898e8def79dc7888c3fb33b90f077a4
abhi1-2 <abhishek.sarkar698@gmail.com>
2017-02-04 12:55:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element= document.getElementById('aat');
 element.innerHTML="qert";
 var pic=document.getElementById('er');
 pic.Onclick=function(){
-  img.style.marginLeft='100px';  
+  pic.style.marginLeft='100px';  
 };
