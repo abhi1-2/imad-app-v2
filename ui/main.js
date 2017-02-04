0d9ce69d1116486ae8cdf32a47ae582e9269ba48
abhi1-2 <abhishek.sarkar698@gmail.com>
2017-02-04 12:55:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element= document.getElementById('aat');
 element.innerHTML="qert";
 var pic=document.getElementById('er');
-pic.onclick=function(){
+pic.Onclick=function(){
   img.style.marginLeft='100px';  
 };
